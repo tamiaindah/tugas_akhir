0aa706981c589bc863b52fcbec79a16f956f838a
tamiaindah <tamiaindah66000@gmail.com>
2022-02-09 08:23:42 +0700

    nambahin api

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://hanaflowerstory.com/api'
 })
 export default Api
